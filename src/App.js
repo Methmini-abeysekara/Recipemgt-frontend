83c6b845f63e812d805d278b9e23ca7ca3d6d349
Methmini-abeysekara <manojaa415@gmail.com>
2022-01-21 13:11:31 +0530

    Recipe  management frontend

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import CounterClass from './components/CounterClass';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <div >
+
+      <Header></Header>
+      
+
+    </div>
+  );
+}
+
+export default App;
